188282211745fc6b50466e38a878e3b0305b1a75
Piotr Korgol <piotr.korgol@gmail.com>
2013-07-03 17:19:32 +0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name	"Piotr Korgol"
+    email	"piotr.korgol@gmail.com"
+    password	"martamarta"
+    password_confirmation  "martamarta"
+  end
+end
